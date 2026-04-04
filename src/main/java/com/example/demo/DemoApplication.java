b8bf26fb074e8b07c4fcfe3a18d331670322dc1f
Vidhan <vidhansharma01@gmail.com>
2026-04-04 11:45:40 +0530

    Word count apache flink example

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,10 +2,8 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.retry.annotation.EnableRetry;
 
-@SpringBootApplication
-@EnableRetry
+@SpringBootApplication(scanBasePackages = "com.example")
 public class DemoApplication {
 
 	public static void main(String[] args) {
